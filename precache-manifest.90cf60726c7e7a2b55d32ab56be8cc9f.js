8dee8cf7bdaf36f6386536ac2881a17008188905
Roshan Saud <rosansaud5@mail.com>
2020-10-28 17:45:21 +0545

    Updates

---
+++ precache-manifest.90cf60726c7e7a2b55d32ab56be8cc9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1954f4401c7b8e59b4b7705f7d5e5ae0",
+    "url": "/schedulerApp/index.html"
+  },
+  {
+    "revision": "a71b854c9e6f59b1117d",
+    "url": "/schedulerApp/static/css/main.9753e8fb.chunk.css"
+  },
+  {
+    "revision": "29ff43855b4157469d07",
+    "url": "/schedulerApp/static/js/2.4369aa54.chunk.js"
+  },
+  {
+    "revision": "3fc67584a795169bc77165cef3b6afa4",
+    "url": "/schedulerApp/static/js/2.4369aa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71b854c9e6f59b1117d",
+    "url": "/schedulerApp/static/js/main.833fdd3a.chunk.js"
+  },
+  {
+    "revision": "dc7f68220c4b466e6f26",
+    "url": "/schedulerApp/static/js/runtime-main.0337f730.js"
+  },
+  {
+    "revision": "871bbe237987bb748535a30628ec211e",
+    "url": "/schedulerApp/static/media/clock.871bbe23.svg"
+  },
+  {
+    "revision": "b91becec3f27de987b62b0949fa55c48",
+    "url": "/schedulerApp/static/media/logo_icon.b91becec.svg"
+  },
+  {
+    "revision": "05e8a417c5c602c25867edaabd3b6f92",
+    "url": "/schedulerApp/static/media/option_icon.05e8a417.svg"
+  }
+]);
